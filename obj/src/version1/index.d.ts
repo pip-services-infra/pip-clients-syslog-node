cd46876947b76bfa5bf481d934926c94d632309c
Sergey Seroukhov <seroukhov@gmail.com>
2017-04-13 08:56:17 -0700

    Added missing export

--- obj/src/version1/index.d.ts
+++ obj/src/version1/index.d.ts
@@ -3,6 +3,7 @@ export { EventLogTypeV1 } from './EventLogTypeV1';
 export { SystemEventV1 } from './SystemEventV1';
 export { IEventLogClientV1 } from './IEventLogClientV1';
 export { EventLogHttpClientV1 } from './EventLogHttpClientV1';
+export { EventLogLambdaClientV1 } from './EventLogLambdaClientV1';
 export { EventLogSenecaClientV1 } from './EventLogSenecaClientV1';
 export { EventLogDirectClientV1 } from './EventLogDirectClientV1';
 export { EventLogNullClientV1 } from './EventLogNullClientV1';
